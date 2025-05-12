a64ce116602b1f571702c37983ab4669d7f3baed
Timofey <dooezzz@gmail.com>
2025-05-12 22:24:29 +0300

    Yazi searchjump.

--- dot_config/yazi/init.lua
+++ dot_config/yazi/init.lua
@@ -1,10 +1,14 @@
-require("keyjump"):setup({
-	icon_fg = "#fda1a1",
-	first_key_fg = "#df6249",
-	go_table = { -- `g` to open go menu(global mode)
-		{ on = { "h" }, run = "cd ~",           desc = "Go to home" },
-		{ on = { "c" }, run = "cd ~/.config",   desc = "Go to config" },
-		{ on = { "u" }, run = "cd /media/UUI/", desc = "Go to Mobile disk" },
-	}
+require("searchjump"):setup({
+	unmatch_fg = "#736e6b",
+	match_str_fg = "#82a2d9",
+	match_str_bg = "#242730",
+	first_match_str_fg = "#000000",
+	first_match_str_bg = "#73AC3A",
+	label_fg = "#92bf8e",
+	label_bg = "#242730",
+	only_current = false,
+	show_search_in_statusbar = false,
+	auto_exit_when_unmatch = false,
+	enable_capital_label = true,
+	search_patterns = ({ "hell[dk]d", "%d+.1080p", "第%d+集", "第%d+话", "%.E%d+", "S%d+E%d+", })
 })
-
